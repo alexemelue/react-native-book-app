4ee470abfb7430ac305acecd68bf37146b57f16c
Alex Emelue <alexemelue@gmail.com>
2025-06-28 18:20:03 -0700

    newly Updated

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -10,8 +10,8 @@ const router = express.Router();
 router.post("/", protectRoute, async (req, res) => {
   try {
     const { title, caption, rating, image } = req.body;
-    // if (!title || !caption || !rating || !image) {
-    if (!title || !caption || !rating) {
+
+    if (!title || !caption || !rating || !image) {
       return res.status(400).json({ message: "Please Provide All Fields!" });
     }
 
@@ -21,14 +21,14 @@ router.post("/", protectRoute, async (req, res) => {
     //   chunk_size: 120000000, // 60MB chunk size (adjust as needed)
     //   public_id: "my_large_file",
     // });
-    // const uploadResponse = await cloudinary.uploader.upload(image);
-    // const imageUrl = uploadResponse.secure_url;
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
     // save to the database
     const newBook = new Book({
       title,
       caption,
       rating,
-      // image: imageUrl,
+      image: imageUrl,
       user: req.user._id,
     });
     newBook.save();
