418fd775542dcbd051e0e7540e1a5f3704caec9b
Alex Emelue <alexemelue@gmail.com>
2025-07-05 12:39:00 -0700

    debug completed

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -82,10 +82,9 @@ router.get("/user", protectRoute, async (req, res) => {
   }
 });
 // delete books
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", protectRoute, async (req, res) => {
   try {
     const book = await Book.findById(req.params.id);
-    console.log(book);
     if (!book) return res.status(404).json({ message: "Book Not Found!" });
     // check if user is the creator of the book
     if (book.user.toString() !== req.user._id.toString())
